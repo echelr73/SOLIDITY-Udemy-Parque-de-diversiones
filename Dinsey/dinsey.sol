790fe9390d342ff0ed561eae0b05fe8e8dfb3f02
echelr73 <eche_lr73@hotmail.com>
2022-12-29 11:34:05 -0300

    Declaraciones iniciales

--- Dinsey/dinsey.sol
+++ Dinsey/dinsey.sol
@@ -4,5 +4,23 @@ pragma experimental ABIEncoderV2;
 import "./ERC20.sol";
 
 contract Dinsey{
-    
+
+    // Instancia del contrato token
+    ERC20Basic private token;
+    //Direccion de Dinsey(owner)
+    address payable public owner;
+
+    constructor () public {
+        token = new ERC20Basic(10000);
+        owner = payable(msg.sender);
+    }
+
+    // Estructura de datos para almacenar los clientes de Dinsey
+    struct cliente{
+        uint tokens_comprados;
+        string[] atracciones_disfrutadas;
+    }
+
+    // Mapping para el registro de clientes
+    mapping (address => cliente) public Clientes;
 }
