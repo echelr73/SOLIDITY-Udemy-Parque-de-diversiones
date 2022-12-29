0c2bfbf7f4262a4b643d558cd48467fea7382a6f
echelr73 <eche_lr73@hotmail.com>
2022-12-29 11:39:40 -0300

    Establecer precio de token

--- Dinsey/dinsey.sol
+++ Dinsey/dinsey.sol
@@ -5,6 +5,8 @@ import "./ERC20.sol";
 
 contract Dinsey{
 
+    // ------------ DECLARACIONES INICIALES --------------------------
+
     // Instancia del contrato token
     ERC20Basic private token;
     //Direccion de Dinsey(owner)
@@ -23,4 +25,12 @@ contract Dinsey{
 
     // Mapping para el registro de clientes
     mapping (address => cliente) public Clientes;
+
+    // ------------ GESTION DE TOKEN --------------------------
+
+    // Function para establecer el precio de un token
+    function PrecioToken(uint _numToken) internal pure returns(uint){
+        //Devuelve la conversion de token a ether 1 token= 1 ether
+        return _numToken * (1 ether);
+    }
 }
